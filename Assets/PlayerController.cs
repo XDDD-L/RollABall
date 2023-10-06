c3f592f2656cc9a08004fa463d54ac10e50f8508
XDDD-L <395226519@qq.com>
2023-10-06 09:00:15 +0800

    win

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -2,12 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
     public Vector2 moveValue;
     public float speed;
     private int count;
+    private int numPickups = 5;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI winText;
+
+    void Start()
+    {
+        count = 0;
+        winText.text = " ";
+        SetCountText();
+    }
 
     void OnMove(InputValue value)
     {
@@ -25,13 +37,16 @@ public class PlayerController : MonoBehaviour
         {
             other.gameObject.SetActive(false);
             count++;
-            Debug.Log(count);
+            SetCountText();
+        }
+    }
+    private void SetCountText()
+    {
+        scoreText.text = "Score: " + count.ToString();
+        if (count >= numPickups)
+        {
+            winText.text = "You win!";
         }
     }
 
-    private void Start()
-    {
-        count = 0;
-        Debug.Log(count);
-    }
 }
